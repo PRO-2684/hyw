5d5edf1d4989f3c101d3c500833e7f82e9dc9d8b
PRO-2684 <54608551+PRO-2684@users.noreply.github.com>
2025-12-25 19:41:02 +0800

    perf: Further prior filtering

--- hyw-base/src/main.rs
+++ hyw-base/src/main.rs
@@ -4,5 +4,8 @@ fn main() {
     println!("HE: {} chars", HE.len());
     println!("YI: {} chars", YI.len());
     println!("WEI: {} chars", WEI.len());
-    println!("Total: {} chars", Hyw::all().size_hint().0);
+
+    let size = Hyw::all().size_hint().0;
+    println!("Total: {size} combinations");
+    println!("Approx. embedding size: {}MB", 4 * size / 1024); // 4 bytes per f32, 1024 floats per embedding
 }
